6dd10e372d0b6b8fa33206d75f942c182aee7c69
KimYasin <kimyaks02@gmail.com>
2025-03-03 12:42:01 +0300

    Refactored code with arrow functions-concise body without return.

--- src/App.js
+++ src/App.js
@@ -20,67 +20,65 @@ const list = [
   },
 ];
 
+//When using arrow functions:
+//Allowed: const item => {....}
+//Allowed(Recommended): const (item) => {...}
+//Not Allowed: const item, index => {....}
+//Allowed(Recommended): const (item, index) => {....}
+//If the function's only purpose is to return a value and there is no business logic between, you can remove the curly "braces" and "return" of the function like App, Search and Link components
+
 //Parent/root Component - Definition of App component
-const App = () => {
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    { /*1st child/leaf Component of App*/}
+    <Search />
 
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      { /*1st child/leaf Component of App*/}
-      <Search />
+    <hr />
+    { /*2nd child/leaf Component of App*/}
+    { /*This is the first instance of a List component*/}
+    <List />
+    { /*This is another instance of a List component*/}
+    <List />
 
-      <hr />
-      { /*2nd child/leaf Component of App*/}
-      { /*This is the first instance of a List component*/}
-      <List />
-      { /*This is another instance of a List component*/}
-      <List />
-
-      { /*Search and List are sibling components*/}
-    </div >
-  );
-}
+    { /*Search and List are sibling components*/}
+  </div >
+);
 
 {/*Item is a child component of List*/ }
 { /*Definition of List component*/ }
-function List() {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.url}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>{item.title}</span>
-          </li>
-        );
-      })}
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.url}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>{item.title}</span>
+      </li>
+    ))}
 
-      {/*BEGIN ---- AVOID DOING THIS*/}
-      {/*{list.map(function (item, index) {
+    {/*BEGIN ---- AVOID DOING THIS*/}
+    {/*{list.map(function (item, index) {
           return (
             <li key={index}>
               call the list from here
             </li>
           );
         })}*/}
-      {/*END --- AVOID DOING THIS*/}
-    </ul>
-  );
-}
+    {/*END --- AVOID DOING THIS*/}
+  </ul>
+);
 
 { /*Definition of Search component*/ }
-function Search() {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  )
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+)
 
 export default App;

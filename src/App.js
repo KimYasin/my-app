d8888e3380852c7cf32335abf039046f6996812e
KimYasin <kimyaks02@gmail.com>
2025-03-04 00:33:12 +0300

    Using props

--- src/App.js
+++ src/App.js
@@ -1,79 +1,74 @@
-//Used to implement React Components & also implement the application
 import React from 'react';
+//This is  global scope
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Kim Yasin',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-//When using arrow functions:
 //Allowed: const item => {....}
 //Allowed(Recommended): const (item) => {...}
 //Not Allowed: const item, index => {....}
 //Allowed(Recommended): const (item, index) => {....}
 //If the function's only purpose is to return a value and there is no business logic between, you can remove the curly "braces" and "return" of the function like App, Search and Link components
 
-//Parent/root Component - Definition of App component
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    { /*1st child/leaf Component of App*/}
-    <Search />
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Kim Yasin',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-    <hr />
-    { /*2nd child/leaf Component of App*/}
-    { /*This is the first instance of a List component*/}
-    <List />
-    { /*This is another instance of a List component*/}
-    <List />
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
 
-    { /*Search and List are sibling components*/}
-  </div >
-);
+      <hr />
 
-{/*Item is a child component of List*/ }
-{ /*Definition of List component*/ }
-const List = () => (
+      <List list={stories} />
+
+    </div >
+  );
+};
+
+{ /*->List component function signature
+    ->since stories is not used in the App component directly, but in one of its child components,
+we passed it as "props" to the List component
+    ->We could also define stories directly in the List component and would
+   not need to use props in the first place; however, in the future we will make
+use of the stories in the App component and thus will keep it there*/ }
+
+{ /*information (props) can only be passed from a parent to a child
+component and not vice versa.*/}
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.url}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>{item.title}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
-
-    {/*BEGIN ---- AVOID DOING THIS*/}
-    {/*{list.map(function (item, index) {
-          return (
-            <li key={index}>
-              call the list from here
-            </li>
-          );
-        })}*/}
-    {/*END --- AVOID DOING THIS*/}
   </ul>
 );
 
-{ /*Definition of Search component*/ }
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.url}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+    <span>{props.item.title}</span>
+  </li>
+);
+
 const Search = () => {
   //add function to handle change event in input field a.k.a event handler in React
   const handleChange = (event) => {
